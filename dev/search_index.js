b16ebaabd8e844327092e99db2342d493ed9db0d
Documenter.jl <documenter@juliadocs.github.io>
2023-10-17 03:31:58 +0000

    build based on e41c729

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HJB","category":"page"},{"location":"#HJB","page":"Home","title":"HJB","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HJB.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HJB]","category":"page"},{"location":"#HJB.HJBProblem","page":"Home","title":"HJB.HJBProblem","text":"encodes the problem\n\ndV/dt + H(t, x, V, dV/dx, p) = 0\nV(tf, x) = l(x)\n\nover the interval tspan=(t0, tf)\n\n\n\n\n\n","category":"type"}]
+}
